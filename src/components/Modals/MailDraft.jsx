d348df769862747a895ec8b6cb3d4f2d53326403
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-07-01 19:25:02 +0530

    copy text in mail draft ...adi 1july24

--- src/components/Modals/MailDraft.jsx
+++ src/components/Modals/MailDraft.jsx
@@ -5,8 +5,49 @@ import Modal from "react-bootstrap/Modal";
 import TextField from "@mui/material/TextField";
 import { FaRegCopy } from "react-icons/fa6";
 
+import copy from "copy-to-clipboard";
+
+import { toast } from "react-toastify";
 export default function MailDraft(props) {
+  const [mailDraftData, setMailDraftData] = useState({
+    to: "ABC Agency",
+    subject: "Requested documents",
+    message: `Hello,
+                                I hope this message finds you well. As per your request, I am submitting the required documents. Please find attached the following documents:
+                1. Statement in PDF/Excel
+                2. Beneficiary details of UPI/IMPS
+                3. Beneficiary details of UPI Txns
+                4. IP Logs
+                5. Device details
+                If there are any further documents or information required, please let me know, and I will be happy to provide them promptly.
+                Thank you for your assistance.
+
+                Best regards,
+                Kotak Bank Ltd`,
+  });
+
   const handleClose = () => props.setMailDraftModal(false);
+
+  function handleChange(e) {
+    setMailDraftData({
+      ...mailDraftData,
+      [e.target.name]: e.target.value || "",
+    });
+
+    // console.log("eeeeeeee", e.target.name, "---", e.target.value);
+  }
+
+  function copyTextFunc() {
+    copy(mailDraftData.message);
+    let resp = copy(mailDraftData.message);
+    if (resp) {
+      toast.success("Message copied successfully");
+      handleClose();
+    } else {
+      toast.error("Unable to copy message");
+    }
+  }
+
   return (
     <>
       <Modal
@@ -34,7 +75,9 @@ export default function MailDraft(props) {
             }}
             variant="standard"
             style={{ width: "100%" }}
-            defaultValue={"ABC Agency"}
+            defaultValue={mailDraftData.to}
+            name="to"
+            onChange={handleChange}
           />
           {/* space */}
           <div className="p-1"></div>
@@ -49,7 +92,9 @@ export default function MailDraft(props) {
             }}
             variant="standard"
             style={{ width: "100%" }}
-            defaultValue={"Requested documents"}
+            defaultValue={mailDraftData.subject}
+            name="subject"
+            onChange={handleChange}
           />
 
           {/* message */}
@@ -59,24 +104,18 @@ export default function MailDraft(props) {
             multiline
             rows={12}
             style={{ width: "100%", border: "none" }}
-            defaultValue="Hello,
-                                I hope this message finds you well. As per your request, I am submitting the required documents. Please find attached the following documents:
-                1. Statement in PDF/Excel
-                2. Beneficiary details of UPI/IMPS
-                3. Beneficiary details of UPI Txns
-                4. IP Logs
-                5. Device details
-                If there are any further documents or information required, please let me know, and I will be happy to provide them promptly.
-                Thank you for your assistance.
-
-                Best regards,
-                Kotak Bank Ltd"
+            defaultValue={mailDraftData.message}
+            name="message"
+            onChange={handleChange}
           />
 
           <div className="d-flex justify-content-end">
             <Button
               style={{ background: "rgba(0, 56, 116, 1)", border: "none" }}
               className="d-flex flex-row justify-content-between align-items-center"
+              onClick={(e) => {
+                copyTextFunc();
+              }}
             >
               <FaRegCopy />
               <div className="p-1"></div>
